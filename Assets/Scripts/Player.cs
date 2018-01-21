d35654584ce51b83b1663c145c1291fa22c9ecb7
aliadil <aliadilg@gmail.com>
2018-01-21 06:34:28 +0500

    2d platformar Raycasting implemantation

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof (Controller2D))]
+public class Player : MonoBehaviour {
+
+    Controller2D controller;
+
+	// Use this for initialization
+	void Start () {
+		controller = GetComponent<Controller2D>();
+	}
+	
+
+}
