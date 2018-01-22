9d730ff43cd735167e0a69459eb269145a9ba669
aliadil <aliadilg@gmail.com>
2018-01-22 17:58:40 +0500

    player face flip funtion added according to its  direction in it is moving.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,7 +4,7 @@ using System.Collections;
 [RequireComponent(typeof(Controller2D))]
 public class Player : MonoBehaviour
 {
-
+    private bool m_FacingRight = true;
     float moveSpeed = 6;
     float gravity = -20;
     Vector3 velocity;
@@ -20,9 +20,34 @@ public class Player : MonoBehaviour
     {
 
         Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-
+         
         velocity.x = input.x * moveSpeed;
+
+
+        
+
         velocity.y += gravity * Time.deltaTime;
         controller.Move(velocity * Time.deltaTime);
+        if (input.x > 0 && !m_FacingRight)
+        {
+            Flip();
+        }
+        else if (input.x < 0 && m_FacingRight)
+        {
+            Flip();
+        }
+
+        
+
+    }
+    private void Flip()
+    {
+        // Switch the way the player is labelled as facing.
+        m_FacingRight = !m_FacingRight;
+
+        // Multiply the player's x local scale by -1.
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
     }
 }
