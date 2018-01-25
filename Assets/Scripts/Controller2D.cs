42d7d75285399999244f51517ddc856dc165f1b3
aliadil <aliadilg@gmail.com>
2018-01-26 02:59:12 +0500

    Seprated all raycast method in seprate raycast script

--- Assets/Scripts/Controller2D.cs
+++ Assets/Scripts/Controller2D.cs
@@ -1,34 +1,22 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-[RequireComponent(typeof(BoxCollider2D))]
-public class Controller2D : MonoBehaviour
+
+public class Controller2D : RaycastController
 {
-
-    public LayerMask collisionMask;
-
-    const float skinWidth = .015f;
-    public int horizontalRayCount = 4;
-    public int verticalRayCount = 4;
 
     float maxClimbAngle = 80f;
     float maxDescendAngle = 75f;
-
-    float horizontalRaySpacing;
-    float verticalRaySpacing;
-
-    // refrences 
-    BoxCollider2D collider;
-    RaycastOrigins raycastOrigins;
     // Public Refrence 
     public CollisionInfo collisions;
 
-    void Start()
+    //overrides over start method in raycastcontroller
+   public override void Start()
     {
-        collider = GetComponent<BoxCollider2D>();
-        CalculateRaySpacing();
-    }
+       // calling start method of raycastcontroller and then we will continoue over this start method
+        base.Start();
 
+    }
     public void Move(Vector3 velocity)
     {
         UpdateRaycastOrigins();
@@ -200,34 +188,7 @@ public class Controller2D : MonoBehaviour
         }
     }
 
-    void UpdateRaycastOrigins()
-    {
-        Bounds bounds = collider.bounds;
-        bounds.Expand(skinWidth * -2);
 
-        raycastOrigins.bottomLeft = new Vector2(bounds.min.x, bounds.min.y);
-        raycastOrigins.bottomRight = new Vector2(bounds.max.x, bounds.min.y);
-        raycastOrigins.topLeft = new Vector2(bounds.min.x, bounds.max.y);
-        raycastOrigins.topRight = new Vector2(bounds.max.x, bounds.max.y);
-    }
-
-    void CalculateRaySpacing()
-    {
-        Bounds bounds = collider.bounds;
-        bounds.Expand(skinWidth * -2);
-
-        horizontalRayCount = Mathf.Clamp(horizontalRayCount, 2, int.MaxValue);
-        verticalRayCount = Mathf.Clamp(verticalRayCount, 2, int.MaxValue);
-
-        horizontalRaySpacing = bounds.size.y / (horizontalRayCount - 1);
-        verticalRaySpacing = bounds.size.x / (verticalRayCount - 1);
-    }
-
-    struct RaycastOrigins
-    {
-        public Vector2 topLeft, topRight;
-        public Vector2 bottomLeft, bottomRight;
-    }
 
     public struct CollisionInfo
     {
