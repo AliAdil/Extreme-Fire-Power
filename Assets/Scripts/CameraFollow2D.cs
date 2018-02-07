1005ea8eefad315abdbd24af8c7c6d50b9a2114d
aliadil <aliadilg@gmail.com>
2018-02-08 02:50:52 +0500

    Camera follow script added

---
+++ Assets/Scripts/CameraFollow2D.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow2D : MonoBehaviour {
+
+    // to store the target which camera will be following 
+    public Controller2D target;
+    
+    // size which camera will be following 
+    public Vector2 focusAreaSize;
+
+    struct FocusArea
+    {
+        public Vector2 centre;
+        float left, right;
+        float top, bottom;
+
+        public FocusArea(Bounds targetBounds, Vector2 size)
+        {
+            left = targetBounds.center.x - size.x / 2;
+            right = targetBounds.center.x + size.x / 2;
+            bottom = targetBounds.min.y;
+            top = targetBounds.min.y + size.y;
+
+            // getting mid point add and then dividing them 
+            centre = new Vector2((left + right) / 2, (top + bottom) / 2);
+        }
+
+        public void update(Bounds targetBounds)
+        {
+            float shiftX = 0; 
+            if (targetBounds.min.x < left)
+            {
+                shiftX = targetBounds.min.x - left;
+            }
+            else if (targetBounds.max.x > right)
+            {
+                shiftX = targetBounds.max.x - right;
+            }
+        }
+    }
+
+
+}
