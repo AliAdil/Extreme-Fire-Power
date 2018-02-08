29c0d1b82caa623626fd76418c497feea265cedf
aliadil <aliadilg@gmail.com>
2018-02-09 00:17:52 +0500

    Player Input seprate class added

---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent (typeof(Player))]
+public class PlayerInput : MonoBehaviour {
+    // player class refrence 
+    Player player;
+
+	void Start () {
+        player = GetComponent<Player>();
+	}
+	
+	
+	void Update () {
+		
+	}
+}
