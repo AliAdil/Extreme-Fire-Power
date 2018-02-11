7a22a6b81dfb9a448bc6c6a743035960edace76a
aliadil <aliadilg@gmail.com>
2018-02-11 23:06:39 +0500

    PlayerInput Update

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -2,30 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityStandardAssets.CrossPlatformInput;
-[RequireComponent (typeof(Player))]
-public class PlayerInput : MonoBehaviour {
-    // player class refrence 
+[RequireComponent(typeof(Player))]
+public class PlayerInput : MonoBehaviour
+{
+
     Player player;
 
-	void Start () {
+    void Start()
+    {
         player = GetComponent<Player>();
-	}
-	
-	
-	void Update () {
-        //Vector2 directionalInput = new Vector2(CrossPlatformInputManager.GetAxisRaw("Horizontal"), CrossPlatformInputManager.GetAxisRaw("Vertical"));
+    }
+
+    void Update()
+    {
         Vector2 directionalInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-       player.SetDirectionalInput(directionalInput);
+        player.SetDirectionalInput(directionalInput);
 
-       if (Input.GetButtonDown("Jump"))
-       {
-           player.OnJumpInputDown();
-       }
-
-       if (Input.GetButtonUp("Jump"))
-       {
-           player.OnJumpInputUp();
-       }
-		
-	}
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            player.OnJumpInputDown();
+        }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            player.OnJumpInputUp();
+        }
+    }
 }
